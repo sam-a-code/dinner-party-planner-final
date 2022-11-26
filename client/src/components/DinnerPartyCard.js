49076b43c0c2c9f77e32407de2650921e702f811
Sam Avery <sam.avery1@gmail.com>
2022-11-26 08:31:10 -0600

    CSS experiments

--- client/src/components/DinnerPartyCard.js
+++ client/src/components/DinnerPartyCard.js
@@ -6,23 +6,53 @@ import { useNavigate } from "react-router-dom"
 
 function DinnerPartyCard({date, location, vibes, drinks, food, guests}) {
     return (
-        <>
+        <div className="card">
         <h4>DATE: {date}</h4>
         <h4>LOCATION: {location}</h4>
         <h4>VIBES: {vibes.map((item,i) => (
-            <div key={i}>{item.theme} {item.decor} {item.spotify_playlist} {item.games}</div>
+            <div key={i}>
+                Theme: {item.theme}
+                <br></br>
+                Decor: {item.decor}
+                <br></br>
+                <a href={item.spotify_playlist}>Spotify Playlist</a>
+                <br></br>
+                Games:{item.games}
+            </div>
         ))}</h4>
         <h4>DRINKS: {drinks.map((item,i) => (
-            <div key={i}>{item.recipe_name} {item.recipe_link} {item.ingredients}</div>
+            <div key={i}>
+                Recipe name: {item.recipe_name}
+                <br></br>
+                <a href={item.recipe_link}>Recipe link</a>
+                <br></br>
+                Ingredients: {item.ingredients}
+            </div>
         ))}</h4>
         <h4>FOOD: {food.map((item,i) => (
-            <div key={i}>{item.recipe_name} {item.recipe_link} {item.ingredients}</div>
+            <div key={i}>
+                Recipe name: {item.recipe_name}
+                <br></br>
+                <a href={item.recipe_link}>Recipe link</a>
+                <br></br>
+                Ingredients: {item.ingredients}
+            </div>
         ))}</h4>
         <h4>GUESTS: {guests.map((item,i) => (
-            <div key={i}>{item.name} {item.contact} {item.plus_ones} {item.dietary_restrictions} {item.assigned_dishes} {item.rsvp_status}</div>
+            <div key={i}>
+                Name: {item.name}
+                <br></br>
+                Contact info: {item.contact}
+                <br></br>
+                Plus ones: {item.plus_ones}
+                <br></br>
+                Dietary restrictions: {item.dietary_restrictions}
+                <br></br>
+                Assigned dishes: {item.assigned_dishes}
+                <br></br>
+                RSVP Status: {item.rsvp_status}</div>
         ))}</h4>
-
-        </>
+        </div>
     )
 }
 
--- client/src/components/NewDP.js
+++ client/src/components/NewDP.js
@@ -3,9 +3,58 @@ import { useNavigate } from "react-router-dom"
 
 function NewDP() {
     return (
-        <>
+        <div className="new-dinner-party-background">
         <h1>New Dinner Party Template to come!</h1>
-        </>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        </div>
+
     )
 }
 
