b1cfb2f8bcf872c1ac5570fb9ae7d6869a62a0a0
Sam Avery <sam.avery1@gmail.com>
2022-11-28 12:13:08 -0600

    Setting up dinner party view

---
+++ app/serializers/dinner_party_view_serializer.rb
@@ -0,0 +1,10 @@
+class DinnerPartyViewSerializer < ActiveModel::Serializer
+  attributes :id, :date, :location
+
+  has_one :user
+  has_many :vibes
+  has_many :guests
+  has_many :food_menus
+  has_many :drink_menus
+
+end
--- client/src/components/NewDinnerParty.js
+++ client/src/components/NewDinnerParty.js
@@ -1,58 +1,76 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
+import { useForm } from "react-hook-form"
+
 
 function NewDinnerParty() {
+    const [date, setDate] = useState("")
+    const [location, setLocation] = useState("")
+    const [vibes, setVibes] = useState("")
+    const [guests, setGuests] = useState("")
+    const [foodMenu, setFoodMenu] = useState("")
+    const [drinkMenu, setDrinkMenu] = useState("")
+
     return (
         <div className="new-dinner-party-background">
-        <h1>New Dinner Party Template to come!</h1>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+        <h1>New Dinner Party Template!</h1>
+        <form>
+            <label>Date: </label>
+                <input
+                type="datetime"
+                name="date"
+                placeholder="Select date and time"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                />
+                <br></br>
+            <label>Location: </label>
+                <input
+                type="text"
+                name="location"
+                placeholder="Select location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                />
+                <br></br>
+            <label>Vibes: </label>
+                <input
+                type="text"
+                name="vibes"
+                placeholder="Define your party vibe"
+                value={vibes}
+                onChange={(e) => setVibes(e.target.value)}
+                />
+                <br></br>
+            <label>Guests: </label>
+                <input
+                type="text"
+                name="guests"
+                placeholder="Who is coming to your party?"
+                value={guests}
+                onChange={(e) => setGuests(e.target.value)}
+                />
+                <br></br>
+            <label>Food menu: </label>
+                <input
+                type="text"
+                name="food menu"
+                placeholder="What do you plan to cook?"
+                value={foodMenu}
+                onChange={(e) => setFoodMenu(e.target.value)}
+                />
+                <br></br>
+            <label>Drink menu: </label>
+                <input
+                type="text"
+                name="drink menu"
+                placeholder="What do you plan to drink?"
+                value={drinkMenu}
+                onChange={(e) => setDrinkMenu(e.target.value)}
+                />
+                <br></br>
+
+        </form>
         </div>
 
     )
